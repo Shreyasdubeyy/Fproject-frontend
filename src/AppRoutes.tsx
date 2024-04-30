6dd082862da119c80ac75e5d96653b74cf23bdd2
unknown <shreyasdubey03@gmail.com>
2024-05-01 01:37:03 +0530

    adding user profile form features

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes =()=>{
     return(
@@ -14,6 +15,8 @@ const AppRoutes =()=>{
            {/* to avoid access to user before loging in */}
            <Route element={<ProtectedRoute />}>
            <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
+           <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage /></Layout>} />
+
            </Route>
            
             <Route path="*" element={<Navigate to="/" />} />
