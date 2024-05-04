07c6420e5e53cc0133ffde8be427a1cee395f53e
unknown <shreyasdubey03@gmail.com>
2024-05-05 04:59:05 +0530

    added checkout flow

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
 
 const AppRoutes =()=>{
     return(
@@ -14,6 +15,15 @@ const AppRoutes =()=>{
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
            
            <Route path = "/search/:city" element={<Layout showHero ={false}><SearchPage /></Layout>}/>
+
+           <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      />
 
            {/* to avoid access to user before loging in */}
            <Route element={<ProtectedRoute />}>
--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -17,16 +17,18 @@ const formSchema = z.object({
 });
 
 //automatically determine typescehma
-type UserFormData = z.infer<typeof formSchema>;
+export type UserFormData = z.infer<typeof formSchema>;
 
 
 type Props = {
     currentUser: User;
   onSave: (userProfileData: UserFormData)=> void;
   isLoading: boolean;
+  title?: string;
+  buttonText?: string;
 };
 
-const UserProfileForm = ({ onSave , isLoading , currentUser }: Props) => {
+const UserProfileForm = ({ onSave , isLoading , currentUser , title = "User Profile" , buttonText = "Submit" }: Props) => {
   const form =useForm<UserFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
@@ -43,7 +45,7 @@ useEffect(() => {
         className="space-y-4 bg-gray-50 rounded-lg md:p-10"
         >
             <div>
-                <h2 className="text-2xl font-bold">User Profile Details</h2>
+                <h2 className="text-2xl font-bold">{title}</h2>
                 <FormDescription>
                     View and Change your profile information here
                 </FormDescription>
@@ -125,7 +127,7 @@ useEffect(() => {
             <Button 
             type="submit" 
             className="bg-orange-500">
-                Submit
+                {buttonText}
                 </Button>}
 
         </form>
